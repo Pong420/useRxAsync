329203a47ffecb870f506abdebd34f39d210f9ec
Pong <samfunghp@gmail.com>
2019-10-29 23:08:39 +0800

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,23 @@
 module.exports = {
-  extends: 'react-app',
   env: {
+    es6: true,
     node: true,
-    browser: true,
-    jest: true,
   },
+  extends: ['plugin:@typescript-eslint/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['react-hooks', '@typescript-eslint'],
   rules: {
-    '@typescript-eslint/no-angle-bracket-type-assertion': 0,
     '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 };
